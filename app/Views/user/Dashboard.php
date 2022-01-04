7dd0c7cc3b3f42edeff252bcd39a3fc08a80451e
enjoymylif <noru0916@gmail.com>
2022-01-04 10:32:57 +0500

    Read description :100:
    1. Added `FontAwesome` for icons.
    2. Design changes in `User` dashboard.

--- app/Views/user/Dashboard.php
+++ app/Views/user/Dashboard.php
@@ -1,3 +1,15 @@
-<div class="flex justify-between items-center px-4 py-6 bg-gray-50 h-24">
-    <span class="text-2xl flex font-semibold"><?= $subTitle ?></span>
+
+<div class="flex flex-col w-full">
+    <div class="flex justify-between w-full items-center h-16">
+        <div class="flex items-center w-full mr-4">
+            <i class="fas fa-search text-gray-400 ml-4"></i>
+            <input class="w-full px-2 py-4 h-full outline-none" placeholder="Search..."></input>
+        </div>
+        <div class="mx-4">
+            <a href="<?= base_url('/login/logout') ?>">Logout</a>
+        </div>
+    </div>
+    <div class="text-2xl flex font-semibold border-t px-4 py-4">
+        <?= $subTitle ?>
+    </div>
 </div>
